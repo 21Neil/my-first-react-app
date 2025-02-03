1a45f5fc9089cc81d5e72932cfe1bef3ddf334f3
21Neil <88711171+21Neil@users.noreply.github.com>
2025-02-03 15:11:50 +0800

    feat: export and import components

---
+++ src/FavoriteFood.jsx
@@ -0,0 +1,13 @@
+function Apple() {
+  return (
+    <div>Apple!</div>
+  )
+}
+
+function Steak() {
+  return (
+    <div>Steak!</div>
+  )
+}
+
+export {Apple, Steak}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Greeting from './Greeting.jsx'
+import { Apple, Steak } from './FavoriteFood.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting />
+    <Apple />
+    <Steak />
   </StrictMode>,
 )
